94b5b5c79d3e8e983b3b54c42e367d33b016d6fb
Omnical <omnical@outlook.com>
2025-06-17 23:15:29 +1000

    React-bootstrap header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,47 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button"
+
+function Header() {
+  return (
+    <Navbar bg="dark" data-bs-theme="dark">
+      <Container>
+        <Navbar.Brand href="#home">
+            <img
+              src="/vite.svg"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+          </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link className="text-secondary" href="#home">Home</Nav.Link>
+            <Nav.Link className="text-white" href="#features">Features</Nav.Link>
+            <Nav.Link className="text-white" href="#pricing">Pricing</Nav.Link>
+            <Nav.Link className="text-white" href="#faqs">FAQs</Nav.Link>
+            <Nav.Link className="text-white" href="#about">About</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search..."
+              className="me-2"
+              aria-label="Search"
+            />
+          </Form>
+          <Navbar.Text>
+            <Button variant="outline-light" className="me-2">Login</Button>
+            <Button variant="warning">Signup</Button>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Header;
