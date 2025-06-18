b914a10fa34b52cbf8604ef984247b83dab644b1
Omnical <omnical@outlook.com>
2025-06-18 17:03:51 +1000

    Hero Section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,37 @@
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
 function Home() {
   return (
     <>
+      <Container className="px-4 p5 my-5 text-center">
+        <img
+          className="d-block mx-auto mb-4"
+          src="vite.svg"
+          alt=""
+          width="72"
+          height="57"
+        />
+        <h1 className="display-5 fw-bold text-body-emphasis">Centered hero</h1>
+        <Col lg={6} className="mx-auto">
+          <p className="lead mb-4">
+            Quickly design and customize responsive mobile-first sites with
+            Bootstrap, the world's most popular front-end open source toolkit,
+            featuring Sass variables and mixins, responsive grid system,
+            extensive prebuilt components, and powerful JavaScript plugins.
+          </p>
+          <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <Button variant="primary" size="lg" className="px-4 gap-3">
+              Primary button
+            </Button>
+            <Button variant="outline-secondary" size="lg" className="px-4">
+              Secondary
+            </Button>
+          </div>
+        </Col>
+      </Container>
+      {/* Standard Bootstrap converted to components [ standard bootstrap +html can still be used, converting to react-bootstrap is visually cleaner]*/}
       {/*
       <div class="px-4 py-5 my-5 text-center">
         <img
@@ -10,25 +41,23 @@ function Home() {
           width="72"
           height="57"
         />
-        <h1 class="display-5 fw-bold text-body-emphasis">Centered hero</h1>{" "}
+        <h1 class="display-5 fw-bold text-body-emphasis">Centered hero</h1>
         <div class="col-lg-6 mx-auto">
-          {" "}
           <p class="lead mb-4">
             Quickly design and customize responsive mobile-first sites with
             Bootstrap, the world’s most popular front-end open source toolkit,
             featuring Sass variables and mixins, responsive grid system,
             extensive prebuilt components, and powerful JavaScript plugins.
-          </p>{" "}
+          </p>
           <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-            {" "}
             <button type="button" class="btn btn-primary btn-lg px-4 gap-3">
               Primary button
-            </button>{" "}
+            </button>
             <button type="button" class="btn btn-outline-secondary btn-lg px-4">
               Secondary
-            </button>{" "}
-          </div>{" "}
-        </div>{" "}
+            </button>
+          </div>
+        </div>
       </div>*/}
     </>
   );
